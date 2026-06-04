5252bff191d18a5fa06d1bed0d0b507eed6cdf68
GOLIATH-JR\mattj <mattjponsford@gmail.com>
2026-06-04 22:28:52 +0100

    Adding gui to the ting

---
+++ gui/app.py
@@ -0,0 +1,96 @@
+import customtkinter as ctk
+from gui.pulse_orb import PulseOrb
+import queue
+
+class JarvisGui:
+    def __init__(self):
+        self.ui_queue = queue.Queue()
+        self.on_status_changed = None
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
+
+        self.root = ctk.CTk()
+        self.root.title("Jarvis")
+        self.root.geometry("1300x900")
+        
+        self.build_layout()
+        self.process_ui_queue()
+
+    def build_layout(self):
+        self.main_frame = ctk.CTkFrame(self.root)
+        self.main_frame.pack(fill="both", expand= True, padx=20, pady=20)
+
+        self.status_label = ctk.CTkLabel(
+            self.main_frame,
+            text="Idle",
+            font=("Arial", 32, "bold")
+        )
+        self.status_label.pack(pady=20)
+
+        self.pulse_orb = PulseOrb(self.main_frame)
+        self.pulse_orb.canvas.pack(pady=10)
+
+        self.transcipt_box = ctk.CTkTextbox(
+            self.main_frame,
+            font=("Consolas", 15)
+        )
+        self.transcipt_box.pack(fill="both", expand= True, padx=20, pady=20)
+        self.transcipt_box.insert("end", "JARVIS Ready...\n")
+
+        self.context_box = ctk.CTkTextbox(
+            self.main_frame,
+            height=120,
+            font=("Consolas", 14)
+        )
+        self.context_box.pack(fill="x", padx=20, pady=(0,20))
+        self.context_box.insert("end", "Context will show here...\n")
+
+
+    def queue_status(self, status):
+        self.ui_queue.put(("status", status))
+
+    def queue_transcipt(self, transcipt):
+        self.ui_queue.put(("transcipt", transcipt))
+
+    def queue_response(self, response):
+        self.ui_queue.put(("response", response))
+
+    def queue_context(self, context):
+        self.ui_queue.put(("context", context))
+
+    def process_ui_queue(self):
+        while not self.ui_queue.empty():
+            eventtype, payload = self.ui_queue.get()
+
+            if eventtype == "status":
+                self.set_status(payload)
+                self.pulse_orb.set_orb_colour(payload)
+            elif eventtype == "transcipt":
+                self.add_message("You", payload)
+            elif eventtype == "response":
+                self.add_message("Jarvis", payload)
+            elif eventtype == "context":
+                self.add_context(payload)
+
+        self.root.after(50, self.process_ui_queue)
+
+    def set_status(self, status):
+        self.status_label.configure(
+            text=status.replace("_", " ").title()
+        )
+        if status == "idle":
+            self.pulse_orb.stop()
+        else:
+            self.pulse_orb.start()
+
+    def add_message(self, speaker, message):
+        self.transcipt_box.insert("end", f"{speaker}: {message}\n")
+        self.transcipt_box.see("end")
+
+    def add_context(self, context):
+        self.context_box.delete("1.0", "end")
+        self.context_box.insert("end", f"{context}\n")
+        self.context_box.see("end")
+
+    def run(self):
+        self.root.mainloop()
--- main.py
+++ main.py
@@ -1,103 +1,19 @@
-import sounddevice as sd
-import soundfile as sf
-import queue
-import time
-import numpy as np
-from openwakeword.model import Model
-from chatgptclient import chatgptclient
-import urllib3
-from playsound import playsound
-import asyncio
-from api_client import ApiClient
+from assistant import JarvisAssistant
+from gui.app import JarvisGui
+import threading
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-# Get microphone stream
-FRAME_SIZE = 1280
-RATE = 16000
+def main():
+    assistant = JarvisAssistant()
+    gui = JarvisGui()
 
-command_frames = []
-record_until=0
-is_recording_command = False
-wake_detection_cooldown = 0
-wake_latched = False
-recording_started_at = 0
-last_speech_time = 0
-silence_timeout = 2.0
-min_record_time = 1.0
-WAKE_THRESHOLD = 0.5
-RESET_THRESHOLD = 0.2
+    assistant.on_status_changed = gui.queue_status
+    assistant.on_transcript = gui.queue_transcipt
+    assistant.on_response = gui.queue_response
+    assistant.on_context_changed = gui.queue_context
 
-owwModel = Model(["hey_jarvis"])
-audio_queue = queue.Queue()
-aiclient = chatgptclient()
-api_client = ApiClient()
+    assistant_thread = threading.Thread(target=assistant.run, daemon=True)
+    assistant_thread.start()
+    gui.run()
 
-def is_speech(audio_int16, threshold = 500):
-    rms = np.sqrt(
-        np.mean(
-            audio_int16.astype(np.float32) ** 2
-        )
-    )
-    return rms > threshold
-
-async def play_beep():
-    await asyncio.to_thread(playsound, "Beep.mp3")
-
-def audio_callback(indata, frames, time, status):
-    if status:
-        print(status)
-    audio_queue.put(indata[:, 0].copy())
-
-with sd.InputStream(
-    channels=1,
-    samplerate=RATE,
-    blocksize=FRAME_SIZE,
-    dtype="float32",
-    callback=audio_callback
-):
-    print("Listenting for wake work")
-    while True:
-        audio_float = audio_queue.get()
-        audio_int16 = (audio_float * 32767).astype(np.int16)
-
-        if is_recording_command:
-            command_frames.append(audio_int16)
-            if is_speech(audio_int16):
-                last_speech_time = time.time()
-            
-            record_duration = time.time() - recording_started_at
-            is_long_enough = record_duration > min_record_time
-            is_silent_long_enough = (time.time() - last_speech_time) > silence_timeout
-
-            if is_long_enough and is_silent_long_enough:
-                is_recording_command = False
-                command_audio = np.concatenate(command_frames)
-                command_frames = []
-                print("Finished Recording")
-                sf.write("command.wav", command_audio, RATE)
-                text = aiclient.speech_to_text()
-                ai_response = aiclient.get_response(text)
-                asyncio.run(api_client.handle_command(ai_response, aiclient))
-                wake_detection_cooldown = time.time() + 2
-            continue
-
-        prediction = owwModel.predict(audio_int16)
-        max_score = max(prediction.values()) if prediction else 0
-
-        if wake_latched:
-            if max_score < RESET_THRESHOLD:
-                wake_latched = False
-            continue
-
-        if time.time() < wake_detection_cooldown:
-            continue
-        for wakeword, score in prediction.items():
-            if score > WAKE_THRESHOLD:
-                wake_latched = True
-                asyncio.run(play_beep())
-                is_recording_command = True
-                command_frames = []
-
-                recording_started_at = time.time()
-                last_speech_time = time.time()
-                break
+if __name__ == "__main__":
+    main()
